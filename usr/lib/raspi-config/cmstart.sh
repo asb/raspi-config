45ed3999abb244aa3b74a9bb9c152f7c0a4a6eb1
Simon Long <simon@raspberrypi.com>
2021-06-10 17:04:13 +0100

    Remove R parameter from xcompmgr

--- usr/lib/raspi-config/cmstart.sh
+++ usr/lib/raspi-config/cmstart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if grep -q okay /proc/device-tree/soc/v3d@7ec00000/status 2> /dev/null || grep -q okay /proc/device-tree/soc/firmwarekms@7e600000/status 2> /dev/null ; then
     if ps ax | grep -v grep | grep -q openbox ; then
-        exec xcompmgr -aR
+        exec xcompmgr -a
     fi
 fi
