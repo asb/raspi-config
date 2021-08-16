e8d0a860ec4fcdfc1c7babf92497c630af531eff
Simon Long <simon@raspberrypi.com>
2021-08-16 14:03:18 +0100

    Enable xcompmgr with both full and fake KMS on all platforms

--- usr/lib/raspi-config/cmstart.sh
+++ usr/lib/raspi-config/cmstart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if grep -q okay /proc/device-tree/soc/v3d@7ec00000/status 2> /dev/null || grep -q okay /proc/device-tree/soc/firmwarekms@7e600000/status 2> /dev/null ; then
+if grep -q okay /proc/device-tree/soc/v3d@7ec00000/status 2> /dev/null || grep -q okay /proc/device-tree/v3dbus/v3d@7ec04000/status 2> /dev/null || grep -q okay /proc/device-tree/soc/firmwarekms@7e600000/status 2> /dev/null ; then
     if ps ax | grep -v grep | grep -q openbox ; then
         exec xcompmgr -a
     fi
