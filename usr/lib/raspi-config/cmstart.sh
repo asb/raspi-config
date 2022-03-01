3733fd4d9814e209be3f950a757f4ddd8893ca9e
Simon Long <simon@raspberrypi.com>
2022-03-01 11:39:01 +0000

    Use test in raspi-config for KMS rather than hard-coded version

--- usr/lib/raspi-config/cmstart.sh
+++ usr/lib/raspi-config/cmstart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if grep -q okay /proc/device-tree/soc/v3d@7ec00000/status 2> /dev/null || grep -q okay /proc/device-tree/v3dbus/v3d@7ec04000/status 2> /dev/null || grep -q okay /proc/device-tree/soc/firmwarekms@7e600000/status 2> /dev/null ; then
+if raspi-config nonint is_kms ; then
     if ps ax | grep -v grep | grep -q openbox ; then
         exec xcompmgr -a
     fi
